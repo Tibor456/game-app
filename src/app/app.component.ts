5a97b87f4281d678f3c3054962cd8e9434cd1b44
Tibor456 <tiborbudinsky@gmail.com>
2023-02-16 23:47:25 +0100

    Initial Comit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'game-app3';
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
+  onIntervalfired(firedNumber: number) {
+    if (firedNumber % 2 === 0) {
+      this.evenNumbers.push(firedNumber);
+    } else {
+      this.oddNumbers.push(firedNumber);
+    }
+  }
+}
